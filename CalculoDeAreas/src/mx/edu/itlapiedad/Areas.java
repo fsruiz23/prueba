f2a45c080102bf5c7a265069c3dd1e082bb4adc1
PC_08 <PC_08@192.168.1.71>
2018-08-08 10:11:03 -0500

    Cambio a clase de Sebastian

--- CalculoDeAreas/src/mx/edu/itlapiedad/Areas.java
+++ CalculoDeAreas/src/mx/edu/itlapiedad/Areas.java
@@ -4,7 +4,7 @@ public class Areas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Triangulo miTriangulo= new Triangulo(70,30);
+		Triangulo miTriangulo= new Triangulo(70,300);
 		System.out.println("El area es: "+miTriangulo.getArea());
 	}
 
