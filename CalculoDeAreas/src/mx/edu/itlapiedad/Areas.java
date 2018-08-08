ca6a9656db555691c54dc7a5a7d404250bd887eb
sbtn <sbtn@HP-Notebook>
2018-08-08 10:24:19 -0500

    Se agrego un objeto Circulo

--- CalculoDeAreas/src/mx/edu/itlapiedad/Areas.java
+++ CalculoDeAreas/src/mx/edu/itlapiedad/Areas.java
@@ -5,7 +5,9 @@ public class Areas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Triangulo miTriangulo= new Triangulo(70,300);
-		System.out.println("El area es: "+miTriangulo.getArea());
+		Circulo miCirculo = new Circulo(3);
+		System.out.println("El area del triangulo es: "+miTriangulo.getArea());
+		System.out.println("El area del triangulo es: "+miCirculo.getArea());
 	}
 
 }
